195123c214a23a26d8f78a4efb842333b88d7aef
jersey <lizexi711@gmail.com>
2021-08-18 14:19:58 +0800

    ✨ feat(安装ts):

--- app/renderer/app.tsx
+++ app/renderer/app.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Title from './title'
+
 function App() {
   return (
     <Router>
@@ -9,6 +11,7 @@ function App() {
         <Route path="/">
           <div>可视化简历平台</div>
           <div>这是 Electron + React </div>
+          <Title text="标题" styles={{color: 'red'}} />
         </Route>
       </Switch>
     </Router>
---
+++ app/renderer/title/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface IProps {
+    text: string; // 标题
+    styles?: React.CSSProperties;  // 样式
+}
+
+function Title({
+    text, styles
+}){
+    return(
+        <div style={styles}>{text}</div>
+    )
+}
+
+export default Title;

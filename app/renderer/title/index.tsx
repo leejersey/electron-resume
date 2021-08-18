1e897b23937dc34df9a78846c3e13223ff829919
jersey <lizexi711@gmail.com>
2021-08-18 14:26:47 +0800

    ✨ feat(安装 ESLint + Prettier):

--- app/renderer/title/index.tsx
+++ app/renderer/title/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
 interface IProps {
-    text: string; // 标题
-    styles?: React.CSSProperties;  // 样式
+  text: string; // 标题
+  styles?: React.CSSProperties; // 样式
 }
 
-function Title({
-    text, styles
-}){
-    return(
-        <div style={styles}>{text}</div>
-    )
+function Title({ text, styles }) {
+  return <div style={styles}>{text}</div>;
 }
 
 export default Title;
